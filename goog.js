ef88f02e31858e8e2c3f9f06f3106e7817fe841f
Trevor Edwards <trevedwa@gmail.com>
2020-07-26 00:33:54 -0400

    update i guess

--- goog.js
+++ goog.js
@@ -5,4 +5,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-62099816-2', 'auto');
 ga('send', 'pageview');
-
--- term.js
+++ term.js
@@ -1,77 +1,93 @@
 jQuery(function($, undefined) {
-$('#terminal').terminal(function(command, term) {
+  $('#terminal').terminal(function(command, term) {
     if (command !== '') {
 
-			command = command.trim();
-			var custecho = function(arg){
-				term.echo(arg);
-			}
+      command = command.trim();
+      var custecho = function(arg) {
+        term.echo(arg);
+      }
 
-		switch(command){
-			case 'help':custecho('email: contact me (I\'d love to hear from you!) \ngithub: see my github page \nlinkedin: see my linkedin page \nproject: learn about my past projects \nworkshop: see my steam workshop projects');
-				break;
-			case 'project': custecho('A few of my personal projects are \nbig_red_hacks\nbig_red_app\nred_events\nmaster_speech\nleap_theremin\nlabyrinth\nmelons2melons\nType project [project_name] to find out more!');
-				break;
-			case 'email': custecho('I would love to hear from you! You can contact me at trevedwa@gmail.com');
-				break;
-			case 'github': custecho('Check out my github at https://github.com/TrevorEdwards');
-				break;
-			case 'linkedin': custecho('View my LinkedIn at https://www.linkedin.com/in/trevedwards');
-				break;
-			case 'workshop': custecho('I modded games in high school. See my favorites on my Steam Workshop: http://steamcommunity.com/id/tb002/myworkshopfiles/');
-				break;
-			case 'project big_red_hacks': custecho('I led web development for BigRed//Hacks in Fall 2016! See their latest work: http://bigredhacks.com/');
-				break;
-			case 'project big_red_app': custecho('I maintained an open source Android app for Cornell students.  See it here: https://github.com/TrevorEdwards/bigredapp-android');
-				break;
-			case 'project red_events': custecho('I needed a back-end for Big Red App, so I created the RedEvents API.  See it here: https://github.com/TrevorEdwards/RedEvents');
-				break;
-			case 'project master_speech': custecho('We made a speech-practicing app at BrickHack 2015.  Check it out: http://devpost.com/software/master-speech');
-				break;
-			case 'project leap_theremin': custecho('We made a theremin using a leap motion at my first hackathon, HackBU.  Take a look: http://devpost.com/software/leap-theremin');
-				break;
-			case 'project labyrinth': custecho('One of my mods from high school: players try to escape a maze inhabited by minotaurs.  See it here: https://steamcommunity.com/sharedfiles/filedetails/?id=233263749');
-				break;
-			case 'project melons2melons': custecho('What if you could play pictionary in 3d?  That\'s what I set out to make here: https://steamcommunity.com/sharedfiles/filedetails/?id=173087050');
-				break;
-			case 'arbitrary': term.echo('               .-. \'.-\'\r\n                __\\\/____\r\n              .\'        \'.\r\n             \/            \\         \/\\.\/\\\r\n            \/              \\        |   |\r\n           \/                \\       \\   \/\r\n         ~|      _           |~~~~~~~| |~~~~~\r\n        \/~|     \/ )          |^~~~~^~| |~^~^\/\r\n       \/~~|    (o\/            \\~^~~~\/  \/~^~\/\r\n      \/~^~\\__                  \\__.\'  \/~^~\/\r\n     \/~~~~~~\/________________________\/~^~\/\r\n    \/~^~^~^^~^~^^~^~^~^~^^~^~^~^^~^~^~^~\/\r\n   \/~^~^~^~^^~^~^~^~^^~^~^~^~^^~^~^~^~^\/\r\n   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~', {
-					finalize: function(div) {
-					div.css("color", "blue");
-					}
-				});
-				break;
-			case 'yam': term.echo('               .,;>>%%%%%>>;,.\r\n            .>%%%%%%%%%%%%%%%%%%%%>,.\r\n          .>%%%%%%%%%%%%%%%%%%>>,%%%%%%;,.\r\n        .>>>>%%%%%%%%%%%%%>>,%%%%%%%%%%%%,>>%%,.\r\n      .>>%>>>>%%%%%%%%%>>,%%%%%%%%%%%%%%%%%,>>%%%%%,.\r\n    .>>%%%%%>>%%%%>>,%%>>%%%%%%%%%%%%%%%%%%%%,>>%%%%%%%,\r\n   .>>%%%%%%%%%%>>,%%%%%%>>%%%%%%%%%%%%%%%%%%,>>%%%%%%%%%%.\r\n  .>>%%%%%%%%%%>>,>>>>%%%%%%%%%%\'..`%%%%%%%%,;>>%%%%%%%%%>%%.\r\n .>>%%%>>>%%%%%>,%%%%%%%%%%%%%%.%%%,`%%%%%%,;>>%%%%%%%%>>>%%%%.\r\n >>%%>%>>>%>%%%>,%%%%%>>%%%%%%%%%%%%%`%%%%%%,>%%%%%%%>>>>%%%%%%.\r\n >>%>>>%%>>>%%%%>,%>>>%%%%%%%%%%%%%%%%`%%%%%%%%%%%%%%%%%%%%%%%%%.\r\n >>%%%%%%%%%%%%%%,>%%%%%%%%%%%%%%%%%%%\'%%%,>>%%%%%%%%%%%%%%%%%%%%.\r\n >>%%%%%%%%%%%%%%%,>%%%>>>%%%%%%%%%%%%%%%,>>%%%%%%%%>>>>%%%%%%%%%%.\r\n >>%%%%%%%%;%;%;%%;,%>>>>%%%%%%%%%%%%%%%,>>>%%%%%%>>;\";>>%%%%%%%%%%%.\r\n `>%%%%%%%%%;%;;;%;%,>%%%%%%%%%>>%%%%%%%%,>>>%%%%%%%%%%%%%%%%%%%%%%%%%.\r\n  >>%%%%%%%%%,;;;;;%%>,%%%%%%%%>>>>%%%%%%%%,>>%%%%%%%%%%%%%%%%%%%%%%%%%%.\r\n  `>>%%%%%%%%%,%;;;;%%%>,%%%%%%%%>>>>%%%%%%%%,>%%%%%%\'%%%%%%%%%%%%%%%%%%>>.\r\n   `>>%%%%%%%%%%>,;;%%%%%>>,%%%%%%%%>>%%%%%%\';;;>%%%%%,`%%%%%%%%%%%%%%>>%%>.\r\n    >>>%%%%%%%%%%>> %%%%%%%%>>,%%%%>>>%%%%%\';;;;;;>>,%%%,`%    `;>%%%%%%>>%%\r\n    `>>%%%%%%%%%%>> %%%%%%%%%>>>>>>>>;;;;\'.;;;;;>>%%\'  `%%\'         ;>%%%%%>\r\n     >>%%%%%%%%%>>; %%%%%%%%>>;;;;;;\'\'    ;;;;;>>%%%                  ;>%%%%\r\n     `>>%%%%%%%>>>, %%%%%%%%%>>;;\'        ;;;;>>%%%\'                   ;>%%%\r\n      >>%%%%%%>>>\':.%%%%%%%%%%>>;        .;;;>>%%%%                   ;>%%%\'\r\n      `>>%%%%%>>> ::`%%%%%%%%%%>>;.      ;;;>>%%%%\'                  ;>%%%\'\r\n       `>>%%%%>>> `:::`%%%%%%%%%%>;.     ;;>>%%%%%                  ;>%\r\n        `>>%%%%>>, `::::`%%%%%%%%%%>,   .;>>%%%%%\'                   ;>%\r\n         `>>%%%%>>, `:::::`%%%%%%%%%>>. ;;>%%%%%%                    ;>%\r\n          `>>%%%%>>, :::::::`>>>%%%%>>> ;;>%%%%%\'                     ;>%,\r\n           `>>%%%%>>,::::::,>>>>>>>>>>\' ;;>%%%%%                       ;%%\r\n             >>%%%%>>,:::,%%>>>>>>>>\'   ;>%%%%%.                        ;%%\r\n              >>%%%%>>``%%%%%>>>>>\'     `>%%%%%%.\r\n              >>%%%%>> `@@a%%%%%%\'     .%%%%%%%%%.\r\n              `a@@a%@\'    `%a@@\'       `a@@a%a@@a\'');
-				break;
-			default:
-				try{
-					var result = window.eval(command);
-					if (result !== undefined) {
-						term.echo(new String(result));
-					}
-				} catch(e) {
-					term.echo('Unrecognized command. Type help or spam your tab key.');
-				}
-				break;
-		}
+      switch (command) {
+        case 'help':
+          custecho('email: contact me (I\'d love to hear from you!) \ngithub: see my github page \nlinkedin: see my linkedin page \nproject: learn about my past projects \nworkshop: see my steam workshop projects');
+          break;
+        case 'project':
+          custecho('A few of my personal projects are \nbig_red_hacks\nbig_red_app\nred_events\nmaster_speech\nleap_theremin\nlabyrinth\nmelons2melons\nType project [project_name] to find out more!');
+          break;
+        case 'email':
+          custecho('I would love to hear from you! You can contact me at trevedwa@gmail.com');
+          break;
+        case 'github':
+          custecho('Check out my github at https://github.com/TrevorEdwards');
+          break;
+        case 'linkedin':
+          custecho('View my LinkedIn at https://www.linkedin.com/in/trevedwards');
+          break;
+        case 'workshop':
+          custecho('I modded games in high school. See my favorites on my Steam Workshop: http://steamcommunity.com/id/tb002/myworkshopfiles/');
+          break;
+        case 'project big_red_hacks':
+          custecho('I led web development for BigRed//Hacks in Fall 2016! See their latest work: http://bigredhacks.com/');
+          break;
+        case 'project big_red_app':
+          custecho('I maintained an open source Android app for Cornell students.  See it here: https://github.com/TrevorEdwards/bigredapp-android');
+          break;
+        case 'project red_events':
+          custecho('I needed a back-end for Big Red App, so I created the RedEvents API.  See it here: https://github.com/TrevorEdwards/RedEvents');
+          break;
+        case 'project master_speech':
+          custecho('We made a speech-practicing app at BrickHack 2015.  Check it out: http://devpost.com/software/master-speech');
+          break;
+        case 'project leap_theremin':
+          custecho('We made a theremin using a leap motion at my first hackathon, HackBU.  Take a look: http://devpost.com/software/leap-theremin');
+          break;
+        case 'project labyrinth':
+          custecho('One of my mods from high school: players try to escape a maze inhabited by minotaurs.  See it here: https://steamcommunity.com/sharedfiles/filedetails/?id=233263749');
+          break;
+        case 'project melons2melons':
+          custecho('What if you could play pictionary in 3d?  That\'s what I set out to make here: https://steamcommunity.com/sharedfiles/filedetails/?id=173087050');
+          break;
+        case 'arbitrary':
+          term.echo('               .-. \'.-\'\r\n                __\\\/____\r\n              .\'        \'.\r\n             \/            \\         \/\\.\/\\\r\n            \/              \\        |   |\r\n           \/                \\       \\   \/\r\n         ~|      _           |~~~~~~~| |~~~~~\r\n        \/~|     \/ )          |^~~~~^~| |~^~^\/\r\n       \/~~|    (o\/            \\~^~~~\/  \/~^~\/\r\n      \/~^~\\__                  \\__.\'  \/~^~\/\r\n     \/~~~~~~\/________________________\/~^~\/\r\n    \/~^~^~^^~^~^^~^~^~^~^^~^~^~^^~^~^~^~\/\r\n   \/~^~^~^~^^~^~^~^~^^~^~^~^~^^~^~^~^~^\/\r\n   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~', {
+            finalize: function(div) {
+              div.css("color", "blue");
+            }
+          });
+          break;
+        case 'yam':
+          term.echo('               .,;>>%%%%%>>;,.\r\n            .>%%%%%%%%%%%%%%%%%%%%>,.\r\n          .>%%%%%%%%%%%%%%%%%%>>,%%%%%%;,.\r\n        .>>>>%%%%%%%%%%%%%>>,%%%%%%%%%%%%,>>%%,.\r\n      .>>%>>>>%%%%%%%%%>>,%%%%%%%%%%%%%%%%%,>>%%%%%,.\r\n    .>>%%%%%>>%%%%>>,%%>>%%%%%%%%%%%%%%%%%%%%,>>%%%%%%%,\r\n   .>>%%%%%%%%%%>>,%%%%%%>>%%%%%%%%%%%%%%%%%%,>>%%%%%%%%%%.\r\n  .>>%%%%%%%%%%>>,>>>>%%%%%%%%%%\'..`%%%%%%%%,;>>%%%%%%%%%>%%.\r\n .>>%%%>>>%%%%%>,%%%%%%%%%%%%%%.%%%,`%%%%%%,;>>%%%%%%%%>>>%%%%.\r\n >>%%>%>>>%>%%%>,%%%%%>>%%%%%%%%%%%%%`%%%%%%,>%%%%%%%>>>>%%%%%%.\r\n >>%>>>%%>>>%%%%>,%>>>%%%%%%%%%%%%%%%%`%%%%%%%%%%%%%%%%%%%%%%%%%.\r\n >>%%%%%%%%%%%%%%,>%%%%%%%%%%%%%%%%%%%\'%%%,>>%%%%%%%%%%%%%%%%%%%%.\r\n >>%%%%%%%%%%%%%%%,>%%%>>>%%%%%%%%%%%%%%%,>>%%%%%%%%>>>>%%%%%%%%%%.\r\n >>%%%%%%%%;%;%;%%;,%>>>>%%%%%%%%%%%%%%%,>>>%%%%%%>>;\";>>%%%%%%%%%%%.\r\n `>%%%%%%%%%;%;;;%;%,>%%%%%%%%%>>%%%%%%%%,>>>%%%%%%%%%%%%%%%%%%%%%%%%%.\r\n  >>%%%%%%%%%,;;;;;%%>,%%%%%%%%>>>>%%%%%%%%,>>%%%%%%%%%%%%%%%%%%%%%%%%%%.\r\n  `>>%%%%%%%%%,%;;;;%%%>,%%%%%%%%>>>>%%%%%%%%,>%%%%%%\'%%%%%%%%%%%%%%%%%%>>.\r\n   `>>%%%%%%%%%%>,;;%%%%%>>,%%%%%%%%>>%%%%%%\';;;>%%%%%,`%%%%%%%%%%%%%%>>%%>.\r\n    >>>%%%%%%%%%%>> %%%%%%%%>>,%%%%>>>%%%%%\';;;;;;>>,%%%,`%    `;>%%%%%%>>%%\r\n    `>>%%%%%%%%%%>> %%%%%%%%%>>>>>>>>;;;;\'.;;;;;>>%%\'  `%%\'         ;>%%%%%>\r\n     >>%%%%%%%%%>>; %%%%%%%%>>;;;;;;\'\'    ;;;;;>>%%%                  ;>%%%%\r\n     `>>%%%%%%%>>>, %%%%%%%%%>>;;\'        ;;;;>>%%%\'                   ;>%%%\r\n      >>%%%%%%>>>\':.%%%%%%%%%%>>;        .;;;>>%%%%                   ;>%%%\'\r\n      `>>%%%%%>>> ::`%%%%%%%%%%>>;.      ;;;>>%%%%\'                  ;>%%%\'\r\n       `>>%%%%>>> `:::`%%%%%%%%%%>;.     ;;>>%%%%%                  ;>%\r\n        `>>%%%%>>, `::::`%%%%%%%%%%>,   .;>>%%%%%\'                   ;>%\r\n         `>>%%%%>>, `:::::`%%%%%%%%%>>. ;;>%%%%%%                    ;>%\r\n          `>>%%%%>>, :::::::`>>>%%%%>>> ;;>%%%%%\'                     ;>%,\r\n           `>>%%%%>>,::::::,>>>>>>>>>>\' ;;>%%%%%                       ;%%\r\n             >>%%%%>>,:::,%%>>>>>>>>\'   ;>%%%%%.                        ;%%\r\n              >>%%%%>>``%%%%%>>>>>\'     `>%%%%%%.\r\n              >>%%%%>> `@@a%%%%%%\'     .%%%%%%%%%.\r\n              `a@@a%@\'    `%a@@\'       `a@@a%a@@a\'');
+          break;
+        default:
+          try {
+            var result = window.eval(command);
+            if (result !== undefined) {
+              term.echo(new String(result));
+            }
+          } catch (e) {
+            term.echo('Unrecognized command. Type help or spam your tab key.');
+          }
+          break;
+      }
     } else {
-       term.echo('');
+      term.echo('');
     }
-	}, {
-		completion: [
-			'help',
-			'yam',
-			'arbitrary',
-			'project',
-			'workshop',
-			'linkedin',
-			'email',
-			'github'
-		],
-		greetings: function(callback){
-			const greet = 'Welcome to Trevor\'s Personal Site!  Type help for commands.';
-			callback(greet);
-		},
+  }, {
+    completion: [
+      'help',
+      'yam',
+      'arbitrary',
+      'project',
+      'workshop',
+      'linkedin',
+      'email',
+      'github'
+    ],
+    greetings: function(callback) {
+      const greet = 'Welcome to Trevor\'s Personal Site!  Type help for commands.';
+      callback(greet);
+    },
     name: 'TrevorTerminal', //not necessary
     height: 800,
-    prompt: '\u27B3 '});
+    prompt: '\u27B3 '
+  });
 });
